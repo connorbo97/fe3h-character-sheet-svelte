c95b08cd27fd9494a68cb154ed4b725a10e7941f
Connor Borden <connor.b@taboola.com>
2022-07-01 00:23:39 -0700

    initial impl for stats

---
+++ src/routes/home/stats.svelte
@@ -0,0 +1,60 @@
+<script lang='ts'>
+  import { DEFAULT_PLAYER_STAT, PLAYER_STAT, PLAYER_STAT_TO_LABEL } from '../../constants'
+
+  export let stats = DEFAULT_PLAYER_STAT
+
+  const onPlayerStatRoll = (stat: string) => {
+    const statBuff = Math.floor((stats[stat] - 10) / 2);
+    const rng = Math.floor(Math.random() * 20 + 1)
+    const result = rng + statBuff
+    alert(`${result} = ${rng} + ${statBuff}`);
+  }
+
+</script>
+
+<div class="container">
+	{#each Object.keys(stats) as stat}
+		<div class="stat-container">
+      <button class='label' on:click={() => onPlayerStatRoll(stat)}>{PLAYER_STAT_TO_LABEL[stat]}</button>
+      <span class='value'>{stats[stat] || 0}</span>
+    </div>
+	{/each}
+</div>
+
+<style lang="scss">
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: stretch;
+
+    min-height: 100vh;
+    row-gap: 20px;
+    background-color: blue;
+
+    padding: 10px;
+
+    width: min-content;
+  }
+  .stat-container {
+    display: flex;
+    flex-direction: column;
+    row-gap: 5px;
+    background-color: white;
+
+    text-align: center;
+
+    padding: 5px;
+    border-radius: 5px;
+    
+    button {
+      border: 0;
+      padding: 0;
+      background-color: transparent;
+      &:hover {
+        color: red
+      }
+    }
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { DEFAULT_PLAYER_STAT } from '../constants';
+  import Stats from './home/stats.svelte';
+
+  let playerStats = DEFAULT_PLAYER_STAT
+  let playerName = "Default Name"
+
+</script>
+
+<style lang='scss'>
+  // :global {
+  // }
+  .container {
+    display: grid;
+    grid-template-areas: 
+    "stats main";
+    grid-template-rows: min-content 1fr;
+  }
+  .stats{
+    grid-area: stats;
+  }
+  
+</style>
+
+<div class="container">
+  <div class="stats">
+    <Stats/>
+  </div>
+
+</div>
