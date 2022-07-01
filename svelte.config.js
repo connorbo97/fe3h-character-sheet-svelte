ee68166312ef1462cd1e0bed5aa93126ca264084
Connor Borden <connor.b@taboola.com>
2022-07-01 00:23:23 -0700

    add sass and preprocessor

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,20 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import { resolve } from 'path'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+			resolve: {
+				alias: {
+					$constants: resolve('./src/constants'),
+					$root: resolve('./src'),
+				}
+			}
+		}
 	}
 };
 
