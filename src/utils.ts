69d9c1d23744e9f49dc349502a27af9f7673cd4a
Connor Borden <connor.b@taboola.com>
2022-07-02 00:11:36 -0700

    formatting

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,16 @@
-export const getModifierByPlayerStat = (stat: number) => Math.floor((stat - 10) / 2)
+export const getModifierByPlayerStat = (stat: number) => Math.floor((stat - 10) / 2);
 
 export const rollDice = (n: number) => Math.floor(Math.random() * n + 1);
-export const rollD20 = () => rollDice(20)
+export const rollD20 = () => rollDice(20);
+
+// export const getACFromStatsLoadoutAndCombat = (
+// 	sheet: any,
+// 	loadout: {
+// 		class: any;
+// 		weapon: any;
+// 		skills: any;
+// 	},
+// 	otherBonuses: { terrainMod: number }
+// ) => {
+//   sheet.acBonus
+// };
