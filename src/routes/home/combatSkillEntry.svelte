2e3d1b75e7456be445add96dd90f116ad96f01b6
Connor Borden <connor.b@taboola.com>
2022-07-03 00:39:15 -0700

    implement combat skills

---
+++ src/routes/home/combatSkillEntry.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	export let skill: any;
+	export let feature: any;
+	export let isEquipped: any;
+	export let onToggleCombatSkill: any;
+
+	let hideDescription = true;
+
+	console.log(skill, feature);
+</script>
+
+<div class="container">
+	<div class="skill-container">
+		<button class={isEquipped ? 'equipped' : ''} on:click={() => onToggleCombatSkill(skill)} />
+		<div class="label" on:click={() => (hideDescription = !hideDescription)}>{feature?.label}</div>
+		<div
+			class={`caret ${hideDescription ? 'flip' : ''}`}
+			on:click={() => (hideDescription = !hideDescription)}
+		>
+			v
+		</div>
+	</div>
+	{#if !hideDescription}
+		<div class="description" on:click={() => (hideDescription = true)}>{feature?.description}</div>
+	{/if}
+</div>
+
+<style lang="scss">
+	.container {
+		display: flex;
+		flex-direction: column;
+		row-gap: 5px;
+
+		button {
+			height: 20px;
+		}
+	}
+	.skill-container {
+		display: flex;
+		column-gap: 5px;
+
+		align-items: center;
+	}
+
+	.label {
+		flex: 1;
+		cursor: pointer;
+		font-size: 20px;
+	}
+	.description {
+		padding: 5px;
+		border-radius: 5px;
+		background-color: lightgray;
+		display: flex;
+		font-size: 12px;
+	}
+
+	.equipped {
+		background-color: green;
+	}
+
+	.caret {
+		transition: transform 0.2s linear;
+		padding: 0px 5px;
+		cursor: pointer;
+		margin-bottom: 1px;
+		border: 1px solid black;
+		border-radius: 100%;
+		height: 20px;
+	}
+
+	.flip {
+		transform: rotate(180deg);
+	}
+</style>
--- src/routes/home/combatSkills.svelte
+++ src/routes/home/combatSkills.svelte
@@ -1,7 +1,75 @@
 <script lang="ts">
+	import { CLASS_TO_FEATURES, COMBAT_SKILLS_TO_FEATURES } from 'src/constants';
+	import CombatSkillEntry from './combatSkillEntry.svelte';
+
+	export let customCombatSkills: any;
+
+	export let equippedClass: any;
+	export let unlockedClasses: any;
+	export let unlockedClassesPicks: any;
+
+	export let equippedCombatSkills: any;
+	export let onToggleCombatSkill: any;
+
+	$: unlockedCombatSkillsArr = unlockedClasses.reduce((acc: any, curClass: any) => {
+		const unlockedSkills = CLASS_TO_FEATURES[curClass]?.unlocks?.combatSkills || {};
+		const unlockedSkillsArr = Object.keys(unlockedSkills);
+
+		let pickedSkillsArr: any = [];
+		const pickOne = unlockedSkills?.pickOne;
+
+		if (pickOne) {
+			pickedSkillsArr = pickOne
+				.map((skillArray: any, index: any) => {
+					skillArray[unlockedClassesPicks[curClass]?.unlocks?.combatSkills?.pickOne?.[index]];
+				})
+				.filter((skill) => skill);
+		}
+
+		if (unlockedSkillsArr.length || pickedSkillsArr.length) {
+			return [...acc, ...unlockedSkillsArr, ...pickedSkillsArr];
+		}
+
+		return acc;
+	}, []);
+
+	$: getEquippedClassSkills = () => {
+		const skills = CLASS_TO_FEATURES[equippedClass]?.whenEquipped?.combatSkills || {};
+		const skillsArr = Object.keys(skills);
+
+		let pickedSkillsArr: any = [];
+		const pickOne = skills?.pickOne;
+
+		if (pickOne) {
+			pickedSkillsArr = pickOne
+				.map((skillArray: any, index: any) => {
+					skillArray[
+						unlockedClassesPicks[equippedClass]?.whenEquipped?.combatSkills?.pickOne?.[index]
+					];
+				})
+				.filter((skill) => skill);
+		}
+
+		return [...skillsArr, ...pickedSkillsArr];
+	};
+	$: equippedClassCombatSkills = getEquippedClassSkills();
+	$: classCombatSkills = new Set([...equippedClassCombatSkills, ...unlockedCombatSkillsArr]);
+	$: allCombatSkills = [...Array.from(classCombatSkills), ...Object.keys(customCombatSkills)];
+	$: allCombatSkillFeatures = { ...COMBAT_SKILLS_TO_FEATURES, ...customCombatSkills };
+
+	$: console.log(allCombatSkills, allCombatSkillFeatures);
 </script>
 
-<div class="container">combat skills</div>
+<div class="container">
+	{#each allCombatSkills as skill}
+		<CombatSkillEntry
+			{skill}
+			feature={allCombatSkillFeatures[skill]}
+			isEquipped={equippedCombatSkills.indexOf(skill) !== -1}
+			{onToggleCombatSkill}
+		/>
+	{/each}
+</div>
 
 <style lang="scss">
 	.container {
@@ -12,5 +80,6 @@
 		background-color: slateblue;
 
 		flex: 1;
+		padding: 5px;
 	}
 </style>
