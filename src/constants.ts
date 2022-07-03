72f3ad37152db55305196726acbb8824f7856394
Connor Borden <connor.b@taboola.com>
2022-07-03 00:38:12 -0700

    fix pick one; add dice type; add combat skill features

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,8 @@
 //types
+type Dice = '1d2' | '1d3' | '1d4' | '1d20';
 declare type BasicStat = {
 	[s: string]: number | Array<any> | undefined;
-	pickOne?: Array<any>;
+	pickOne?: Array<Array<any>>;
 };
 
 declare type ClassFeatures = {
@@ -342,6 +343,92 @@ export const COMBAT_SKILLS = {
 	FIENDISH_BLOW: 'FIENDISH_BLOW',
 	HEAL_PLUS: 'HEAL_PLUS'
 };
+type SkillFeatures = {
+	label: string;
+	description: string;
+	acBonus?: number;
+	msBonus?: number;
+	hpBonus?: number;
+	attackBonus?: { [s: string]: Array<number | Dice> };
+	protectionBonus?: number;
+	resilienceBonus?: number;
+	bonusCharges?: { [s: string]: number };
+};
+export const COMBAT_SKILLS_TO_FEATURES: { [s: string]: SkillFeatures } = {
+	MYRMIDON_MASTER: {
+		label: 'Myrmidon Master',
+		description: '+1 AC',
+		acBonus: 1
+	},
+	SOLDIER_MASTER: {
+		label: 'Soldier Master',
+		description: '+1 protection',
+		protectionBonus: 1
+	},
+	FIGHTER_MASTER: {
+		label: 'Fighter Master',
+		description: '+1 crit range if the weapon does not have an innate crit chance'
+	},
+	RANGER_MASTER: {
+		label: 'Ranger Master',
+		description: '+1 to range when shooting from cover'
+	},
+	MONK_MASTER: {
+		label: 'Monk Master',
+		description: '+1 resilience',
+		resilienceBonus: 1
+	},
+
+	MERCENARY_THIEF_SKILL: {
+		label: 'Swift',
+		description: '+1 AC',
+		acBonus: 1
+	},
+	VANTAGE: {
+		label: 'Vantage',
+		description: 'If foe initiates combat, and if HP is ≤ 50%, unit still attacks first'
+	},
+	ANIMAL_WHISPERER: {
+		label: 'Animal Whisperer',
+		description: 'You and adjacent allies may roll saves from being dismounted with advantage'
+	},
+	BULLSEYE: {
+		label: 'Bullseye',
+		description: '+4 to hit',
+		attackBonus: {
+			ANY: [4]
+		}
+	},
+	DEATH_BLOW: {
+		label: 'Death Blow',
+		description: '+2 damage when initiating an attack on already damaged targets'
+	},
+	ARMORED_COMBAT: {
+		label: 'Armored Combat',
+		description: '+1 protection when initiating combat'
+	},
+	GRAPPLER: {
+		label: 'Grappler',
+		description:
+			'Gain the Reposition, Pull back, and Shove combat arts. These do not take up combat art slots'
+	},
+
+	FIENDISH_BLOW: {
+		label: 'Fiendish Blow',
+		description: '+2 damage when attacking an undamaged unit'
+	},
+	HEAL_PLUS: {
+		label: 'Heal Plus',
+		description:
+			'Doubles number of usages of Heal and increases the HP healed by 1d2. Does not stack with Priest class',
+		bonusCharges: {
+			[WEAPONS.HEAL]: 8
+		},
+		attackBonus: {
+			[WEAPONS.HEAL]: ['1d2']
+		}
+	}
+};
 // Class
 
 export const CLASS = {
@@ -427,12 +514,14 @@ export const CLASS_TO_FEATURES: ClassToFeatures = {
 		unlocks: {
 			playerStats: {
 				pickOne: [
-					{
-						[PLAYER_STAT.DEX]: 12
-					},
-					{
-						[PLAYER_STAT.STR]: 12
-					}
+					[
+						{
+							[PLAYER_STAT.DEX]: 12
+						},
+						{
+							[PLAYER_STAT.STR]: 12
+						}
+					]
 				]
 			},
 			combatArts: {
@@ -454,12 +543,14 @@ export const CLASS_TO_FEATURES: ClassToFeatures = {
 		unlocks: {
 			playerStats: {
 				pickOne: [
-					{
-						[PLAYER_STAT.DEX]: 12
-					},
-					{
-						[PLAYER_STAT.STR]: 12
-					}
+					[
+						{
+							[PLAYER_STAT.DEX]: 12
+						},
+						{
+							[PLAYER_STAT.STR]: 12
+						}
+					]
 				]
 			},
 			combatArts: {
@@ -593,22 +684,19 @@ export const CLASS_TO_FEATURES: ClassToFeatures = {
 				[PLAYER_STAT.DEX]: 12,
 				[PLAYER_STAT.WIS]: 12
 			},
-			combatSkills: {
-				[COMBAT_SKILLS.MERCENARY_THIEF_SKILL]: 1
-			},
 			playerSkills: {
 				[PLAYER_SKILL.ANIMAL_HANDLING]: 1
 			},
 			weapons: {
 				[WEAPONS.STEEL_LANCE]: 1,
 				[WEAPONS.PIKE]: 1,
-				pickOne: [WEAPONS.IRON_SWORD, WEAPONS.IRON_AXE, WEAPONS.IRON_BOW]
+				pickOne: [[WEAPONS.IRON_SWORD, WEAPONS.IRON_AXE, WEAPONS.IRON_BOW]]
 			}
 		},
 		whenEquipped: {
 			xpMods: {
 				[WEAPON_TYPE.LANCE]: 1.6,
-				pickOne: [{ weapons: [WEAPON_TYPE.SWORD, WEAPON_TYPE.AXE, WEAPON_TYPE.BOW], xp: 1.3 }]
+				pickOne: [[{ weapons: [WEAPON_TYPE.SWORD, WEAPON_TYPE.AXE, WEAPON_TYPE.BOW], xp: 1.3 }]]
 			},
 			toolProficiencies: {
 				[TOOLS.LOCKPICKING]: 1
@@ -717,14 +805,16 @@ export const CLASS_TO_FEATURES: ClassToFeatures = {
 			hpBonus: 1,
 			playerStats: {
 				pickOne: [
-					{
-						[PLAYER_STAT.STR]: 12,
-						[PLAYER_STAT.DEX]: 14
-					},
-					{
-						[PLAYER_STAT.STR]: 14,
-						[PLAYER_STAT.DEX]: 12
-					}
+					[
+						{
+							[PLAYER_STAT.STR]: 12,
+							[PLAYER_STAT.DEX]: 14
+						},
+						{
+							[PLAYER_STAT.STR]: 14,
+							[PLAYER_STAT.DEX]: 12
+						}
+					]
 				]
 			},
 			combatSkills: {
@@ -755,7 +845,9 @@ export const CLASS_TO_FEATURES: ClassToFeatures = {
 				[PLAYER_STAT.INT]: 14
 			},
 			weapons: {
-				pickOne: [WEAPONS.FIRE, WEAPONS.THUNDER, WEAPONS.BLIZZARD, WEAPONS.WIND, 'DOUBLE_EXISTING']
+				pickOne: [
+					[WEAPONS.FIRE, WEAPONS.THUNDER, WEAPONS.BLIZZARD, WEAPONS.WIND, 'DOUBLE_EXISTING']
+				]
 			}
 		},
 		whenEquipped: {
