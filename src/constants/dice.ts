fe95df195c87e2cb48c673cb599d897e40c58259
Connor Borden <connor.b@taboola.com>
2022-07-04 13:33:39 -0700

    add dice enum

---
+++ src/constants/dice.ts
@@ -0,0 +1,17 @@
+export enum Dice {
+	zero = 0,
+	one = 1,
+	two = 2,
+	three = 3,
+	four = 4,
+	five = 5,
+	six = 6,
+	seven = 7,
+	eight = 8,
+	nine = 9,
+	d2 = '1d2',
+	d3 = '1d3',
+	d4 = '1d4',
+	d5 = '1d5',
+	d20 = '1d20'
+}
