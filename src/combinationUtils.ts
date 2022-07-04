fe0a8b1fd3f11dd8c006df97ffa952c44522b822
Connor Borden <connor.b@taboola.com>
2022-07-03 23:07:33 -0700

    more combination utils

--- src/combinationUtils.ts
+++ src/combinationUtils.ts
@@ -1,4 +1,4 @@
-import { CLASS_TO_FEATURES } from './constants';
+import { CLASS_TO_FEATURES, COMBAT_ARTS, COMBAT_SKILLS } from './constants';
 import memoize from 'memoize-one';
 
 export const calculateAllWeapons = (fullSheet: CharacterSheet, equippedClass: string) =>
@@ -8,7 +8,7 @@ export const calculateAllWeapons = (fullSheet: CharacterSheet, equippedClass: st
 		fullSheet.classXP,
 		equippedClass
 	);
-export const calculateAllWeaponsMemoized = memoize(
+const calculateAllWeaponsMemoized = memoize(
 	(
 		unlockedClasses: Array<string>,
 		customWeapons: object,
@@ -16,7 +16,9 @@ export const calculateAllWeaponsMemoized = memoize(
 		equippedClass: string
 	): AllWeapons => {
 		const customSet: Set<string> = new Set(Object.keys(customWeapons));
-		const equippedClassSet: Set<string> = new Set(Object.keys(customWeapons));
+		const equippedClassSet: Set<string> = new Set(
+			Object.keys(CLASS_TO_FEATURES[equippedClass]?.whenEquipped?.weapons || {})
+		);
 		const classUnlockSet: Set<string> = new Set();
 
 		// weapons from unlocked and mastered classes
@@ -41,3 +43,107 @@ export const calculateAllWeaponsMemoized = memoize(
 		};
 	}
 );
+
+export const calculateAllCombatSkills = (fullSheet: CharacterSheet, equippedClass: string) =>
+	calculateAllCombatSkillsMemoized(
+		fullSheet.unlockedClasses,
+		fullSheet.customCombatSkills,
+		fullSheet.classXP,
+		equippedClass
+	);
+const calculateAllCombatSkillsMemoized = (
+	unlockedClasses: Array<string>,
+	customCombatSkills: object,
+	classXP: XPMap,
+	equippedClass: string
+): AllCombatSkills => {
+	const customSet: Set<string> = new Set(Object.keys(customCombatSkills));
+	const equippedClassSet: Set<string> = new Set(
+		Object.keys(CLASS_TO_FEATURES[equippedClass]?.whenEquipped?.combatSkills || {})
+	);
+	const classUnlockSet: Set<string> = new Set();
+
+	// skills from unlocked and mastered classes
+	unlockedClasses.forEach((curClass: string) => {
+		Object.keys(CLASS_TO_FEATURES[curClass]?.unlocks?.combatSkills || {}).forEach((w) =>
+			classUnlockSet.add(w)
+		);
+		const masteredSkills = CLASS_TO_FEATURES[curClass]?.whenEquipped?.combatSkills;
+
+		if (masteredSkills && classXP?.[curClass]?.mastered) {
+			Object.keys(masteredSkills).forEach((w) => classUnlockSet.add(w));
+		}
+	});
+	const fullSet = new Set([...customSet, ...equippedClassSet, ...classUnlockSet]);
+	return {
+		customSet,
+		equippedClassSet,
+		classUnlockSet,
+		fullSet,
+		fullArray: Array.from(fullSet)
+	};
+};
+
+export const calculateAllCombatArts = (
+	fullSheet: CharacterSheet,
+	equippedClass: string,
+	allCombatSkillsSet: Set<string>
+) =>
+	calculateAllCombatArtsMemoized(
+		fullSheet.unlockedClasses,
+		fullSheet.customWeapons,
+		fullSheet.classXP,
+		equippedClass,
+		allCombatSkillsSet
+	);
+const calculateAllCombatArtsMemoized = (
+	unlockedClasses: Array<string>,
+	customCombatArts: object,
+	classXP: XPMap,
+	equippedClass: string,
+	allCombatSkillsSet: Set<string>
+): AllCombatSkills => {
+	const baseCombatArts = [
+		COMBAT_ARTS.TEMPEST_LANCE,
+		COMBAT_ARTS.WRATH_STRIKE,
+		COMBAT_ARTS.SMASH,
+		COMBAT_ARTS.CURVED_SHOT,
+		COMBAT_ARTS.FADING_BLOW
+	];
+	const customSet: Set<string> = new Set(Object.keys(customCombatArts));
+	const equippedClassSet: Set<string> = new Set(
+		Object.keys(CLASS_TO_FEATURES[equippedClass]?.whenEquipped?.combatArts || {})
+	);
+	const classUnlockSet: Set<string> = new Set();
+
+	// combat arts from unlocked and mastered classes
+	unlockedClasses.forEach((curClass: string) => {
+		Object.keys(CLASS_TO_FEATURES[curClass]?.unlocks?.combatArts || {}).forEach((w) =>
+			classUnlockSet.add(w)
+		);
+		const masteredSkills = CLASS_TO_FEATURES[curClass]?.whenMastered?.combatArts;
+
+		if (masteredSkills && classXP?.[curClass]?.mastered) {
+			Object.keys(masteredSkills).forEach((w) => classUnlockSet.add(w));
+		}
+	});
+
+	const fullSet = new Set([
+		...baseCombatArts,
+		...customSet,
+		...equippedClassSet,
+		...classUnlockSet,
+		// if grappler, add the corresponding combat arts
+		...(allCombatSkillsSet.has(COMBAT_SKILLS.GRAPPLER)
+			? [COMBAT_ARTS.REPOSITION, COMBAT_ARTS.SWAP, COMBAT_ARTS.PULL_BACK, COMBAT_ARTS.SHOVE]
+			: [])
+	]);
+
+	return {
+		customSet,
+		equippedClassSet,
+		classUnlockSet,
+		fullSet,
+		fullArray: Array.from(fullSet)
+	};
+};
