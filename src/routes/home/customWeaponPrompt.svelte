b81738ce96d54aa6b28e4f6265960301e6809166
Connor Borden <connor.b@taboola.com>
2022-07-05 21:28:17 -0700

    suppport clearing custom weapon

--- src/routes/home/customWeaponPrompt.svelte
+++ src/routes/home/customWeaponPrompt.svelte
@@ -9,43 +9,71 @@
 	export let customWeapons: { [s: string]: WeaponFeatures };
 	export let onUpdateCustomWeapons: Function;
 
+	$: alreadyCustom = customWeapons[weapon];
+
 	let reason: string = defaultReason;
 	let input: any;
 
 	onMount(() => {
-		input.select();
+		input?.select();
 	});
 </script>
 
 <div class="container">
-	<h1>Adding custom weapon:</h1>
-	<div class="content">
-		<div>Reason:</div>
-		<textarea
-			bind:this={input}
-			id="custom-weapon-prompt-input"
-			type="text"
-			bind:value={reason}
-			rows={3}
-		/>
-	</div>
-
+	<h1>{alreadyCustom ? 'Adding' : 'Removing'} custom weapon:</h1>
+	{#if !alreadyCustom}
+		<div class="content">
+			<div>Reason:</div>
+			<textarea
+				bind:this={input}
+				id="custom-weapon-prompt-input"
+				type="text"
+				bind:value={reason}
+				rows={3}
+			/>
+		</div>
+	{/if}
+	{#if alreadyCustom}
+		<div class="content">
+			<b>Current Reason:</b>
+			{customWeapons[weapon]?.reason}
+		</div>
+	{/if}
 	<button
+		class="action"
 		on:click={() => {
-			onUpdateCustomWeapons({
-				...customWeapons,
-				[weapon]: {
-					...weaponsToFeatures[weapon],
-					reason: reason
-				}
-			});
+			if (alreadyCustom) {
+				const { [weapon]: removed, ...rest } = customWeapons;
+				onUpdateCustomWeapons(rest);
+			} else {
+				onUpdateCustomWeapons({
+					...customWeapons,
+					[weapon]: {
+						...weaponsToFeatures[weapon],
+						reason: reason
+					}
+				});
+			}
 			close();
-		}}>Click to Submit</button
+		}}>Click to {alreadyCustom ? 'Remove' : 'Submit'}</button
 	>
 </div>
 
 <style lang="scss">
+	.container {
+		display: flex;
+		flex-direction: column;
+		row-gap: 10px;
+	}
 	#custom-weapon-prompt-input {
 		width: 100%;
+	}
+
+	.action {
+		display: flex;
+		justify-content: center;
+		width: 100%;
+		padding: 5px;
+		font-size: 20px;
 	}
 </style>
