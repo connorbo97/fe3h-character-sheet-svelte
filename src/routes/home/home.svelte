0b1e3d661c95c143b289c5ea2fa42354fd1d08a8
Connor Borden <connor.b@taboola.com>
2022-07-09 13:50:28 -0700

    fix style

--- src/routes/home/home.svelte
+++ src/routes/home/home.svelte
@@ -164,7 +164,7 @@
 		display: grid;
 		grid-template-areas: 'stats skills classes arts rest';
 		grid-template-columns: min-content 200px 160px 180px 1fr;
-		grid-template-rows: 100%;
+		grid-template-rows: 1fr;
 		column-gap: 5px;
 
 		height: 100%;
