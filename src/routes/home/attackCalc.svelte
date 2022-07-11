8024407dc380dc42e1109af9600bb6f72ac6ae6b
Connor Borden <connor.b@taboola.com>
2022-07-10 18:07:45 -0700

    fix range calc for combat arts

--- src/routes/home/attackCalc.svelte
+++ src/routes/home/attackCalc.svelte
@@ -216,6 +216,7 @@
 		CLASS_TO_FEATURES[equippedClass]?.whenEquipped?.bonusRange?.[
 			weaponsToFeatures[selectedWeapon]?.type
 		] || 0;
+	$: combatArtRange = allCombatArts.fullFeatures[selectedCombatArt]?.range;
 	$: combatSkillsRangeModifier = equippedCombatSkills.reduce(
 		(acc, skill) =>
 			acc +
@@ -225,7 +226,10 @@
 	);
 	$: optionsRangeModifier = 0;
 	$: calcWeaponRange = () => {
-		let baseWeaponRange = [...(weaponsToFeatures[selectedWeapon]?.range || [1])];
+		let baseWeaponRange = combatArtRange
+			? [...combatArtRange]
+			: // copying b/c of mutation lower down
+			  [...(weaponsToFeatures[selectedWeapon]?.range || [1])];
 
 		if (selectedWeapon === WEAPONS.RESTORE) {
 			baseWeaponRange = [1, 2 + 2 * getModifierByPlayerStat(playerStats[PLAYER_STAT.INT])];
